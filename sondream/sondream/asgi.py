9cb98d07679c6b942af69e00888e130f04d6e20a
SeungjaeLee-uvr <sjlee_7104@kaist.ac.kr>
2026-01-12 13:32:50 +0900

    Setup env

---
+++ sondream/sondream/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sondream project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sondream.settings')
+
+application = get_asgi_application()
